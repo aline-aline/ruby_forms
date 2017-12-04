360204c345763363e1e9546376000305af0694e5
aline-aline <alinecris@gmail.com>
2017-12-04 21:50:34 -0200

    Add factories

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :form do
-    title "MyString"
-    description "MyText"
-    user nil
-    primary_color "MyString"
-    enable false
+    title { FFaker::Lorem.phrase }
+    description { FFaker::Lorem.phrase }
+    user
+    primary_color { "##{FFaker::Color.hex_code}" }
+    enable { FFaker::Boolean.maybe }
   end
 end
